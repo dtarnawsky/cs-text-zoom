b19c1849325cf7d319e92ace5a6872dfa99823d4
damian.ionic <damian@ionic.io>
2024-07-03 12:27:48 -0700

    Initial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.text.zoom',
+  appName: 'cs-text-zoom',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { TextZoom } from '@capacitor/text-zoom';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class HomePage {
+  constructor() { }
+
+  async get() {
+    const result = await TextZoom.get();
+    alert(`Text Zoom Value is ${result.value}`);
+  }
+
+  async getPreferred() {
+    const result = await TextZoom.getPreferred();
+    alert(`Text Zoom Value is ${result.value}`);
+  }
+
+  async set() {
+    const result = await TextZoom.getPreferred();
+    await TextZoom.set({ value: result.value });
+
+  }
+}
